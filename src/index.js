9ed02d433bf111526c7d754464cfa4525cc77a57
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-02-23 11:22:30 -0600

    Punctuation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 console.log(`
-  Hello, I'm Richard B. Kaufman López
+  Hello! I'm Richard B. Kaufman López.
   I'm a web engineer. I love JavaScript and Pugs.
 
   Blog: richardkaufman.xyz
