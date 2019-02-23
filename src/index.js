64ff182270dd33cba32fc215180ab7c74d431956
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-02-23 11:18:15 -0600

    Adds #!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 console.log(`
   Hello, I'm Richard B. Kaufman López
   I'm a web engineer. I love JavaScript and Pugs.
 
-  Blog: https://richardkaufman.xyz
+  Blog: richardkaufman.xyz
   GitHub: https://github.com/sparragus
   Twitter: https://twitter.com/sparragus
 `);
