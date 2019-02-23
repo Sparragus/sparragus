402212eb8308ef20687ce06920c07288ad195fd4
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-02-23 11:10:49 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log(`
+  Hello, I'm Richard B. Kaufman López
+  I'm a web engineer. I love JavaScript and Pugs.
+
+  Blog: https://richardkaufman.xyz
+  GitHub: https://github.com/sparragus
+  Twitter: https://twitter.com/sparragus
+`);
